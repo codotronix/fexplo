5d5da5e91e5d40f9ea87360911c7443abd6c581c
Suman Barick <codotronix@gmail.com>
2020-01-20 00:47:24 +0530

    initial commit

---
+++ fe-src-react/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+// const electron = window.require("electron")
+const fs = window.require("fs")
+const path = window.require('path')
+// const global = window.require('global')
+
+function App() {
+  // fs.readdir(path.resolve('/'), (err, l) => console.log('File List = ', l))
+  // fs.readdir(path.resolve(__dirname), (err, l) => console.log('File List = ', l))
+  // console.log(fs)
+  // console.log(path.resolve(__dirname))
+  // console.log(global)
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
